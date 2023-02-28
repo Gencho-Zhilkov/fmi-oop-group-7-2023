e467df10d3e46038153ba9db941416ba30f10c67
Gencho Zhilkov <gencho.zhilkov@gmail.com>
2023-02-28 03:00:20 +0200

    28.02.2023: fix

--- 2023-02-28/size_matters.cpp
+++ 2023-02-28/size_matters.cpp
@@ -32,36 +32,6 @@ int main() {
 	// Add your code here. Use `sizeof` and `offsetof` to determine the
 	// sizes, offsets, and paddings of the different components of `S`.
 	// ...
-	std::cout << std::endl;
-	std::cout << "sizeof(S) = " << sizeof(S) << std::endl;
-	std::cout << "sizeof(S::sh2) = " << sizeof(S::sh2) << std::endl;
-	std::cout << "sizeof(S::str1) = " << sizeof(S::str1) << std::endl;
-	std::cout << "sizeof(S::dbl) = " << sizeof(S::dbl) << std::endl;
-	std::cout << std::endl;
-	std::cout << "offsetof(ch) = " << offsetof(S, ch) << std::endl;
-	std::cout << "sizeof(ch) = " << sizeof(S::ch) << std::endl;
-	std::cout << std::endl;
-	std::cout << "offsetof(l) = " << offsetof(S, l) << std::endl;
-	std::cout << "sizeof(l) = " << sizeof(S::l) << std::endl;
-	std::cout << std::endl;
-	std::cout << "offsetof(sh1) = " << offsetof(S, sh1) << std::endl;
-	std::cout << "sizeof(sh1) = " << sizeof(S::sh1) << std::endl;
-	std::cout << std::endl;
-	std::cout << "offsetof(str1) = " << offsetof(S, str1) << std::endl;
-	std::cout << "sizeof(str1) = " << sizeof(S::str1) << std::endl;
-	std::cout << std::endl;
-	std::cout << "offsetof(dbl) = " << offsetof(S, dbl) << std::endl;
-	std::cout << "sizeof(dbl) = " << sizeof(S::dbl) << std::endl;
-	std::cout << std::endl;
-	std::cout << "offsetof(sh2) = " << offsetof(S, sh2) << std::endl;
-	std::cout << "sizeof(sh2) = " << sizeof(S::sh2) << std::endl;
-	std::cout << std::endl;
-	std::cout << "offsetof(f) = " << offsetof(S, f) << std::endl;
-	std::cout << "sizeof(f) = " << sizeof(S::f) << std::endl;
-	std::cout << std::endl;
-	std::cout << "offsetof(str2) = " << offsetof(S, str2) << std::endl;
-	std::cout << "sizeof(str2) = " << sizeof(S::str2) << std::endl;
-	std::cout << std::endl;
 
 	return 0;
 }
