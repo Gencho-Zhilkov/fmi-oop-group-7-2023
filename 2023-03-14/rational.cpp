a0b22fa90cedd692f3aa0085d5b0ac0e3ef20d3d
Gencho Zhilkov <gencho.zhilkov@gmail.com>
2023-03-22 15:39:39 +0200

    14.03.2023: Add solutions for vector and rational

--- 2023-03-14/rational.cpp
+++ 2023-03-14/rational.cpp
@@ -4,33 +4,48 @@
 
 
 int gcd(int a, int b) {
-	return b == 0 ? a : gcd(b, a % b);
+    return b == 0 ? a : gcd(b, a % b);
 }
+
 
 class Rational {
 
 private:
-	int numerator;
-	int denominator;
+	int num;
+	int denom;
+
+	// Declared "friend" to be able to access the number's private state!
+	// Arithmetic operators
+	friend Rational operator +(const Rational& left, const Rational& right);
+	friend Rational operator -(const Rational& left, const Rational& right);
+	friend Rational operator *(const Rational& left, const Rational& right);
+	friend Rational operator /(const Rational& left, const Rational& right);
+
+	// Comparison operators
+	friend bool operator ==(const Rational& left, const Rational& right);
+	friend bool operator !=(const Rational& left, const Rational& right);
+	friend bool operator <(const Rational& left, const Rational& right);
+	friend bool operator <=(const Rational& left, const Rational& right);
+	friend bool operator >=(const Rational& left, const Rational& right);
+	friend bool operator >(const Rational& left, const Rational& right);
 
 	// Write a number to a stream (for ex. `std::cout`)
-	// Declared "friend" to be able to access the number's private state!
 	friend std::ostream& operator <<(std::ostream& os, const Rational& r);
 
 public:
 	// Constructor: it can be called with 0, 1, or 2 arguments!.
 	Rational(const int num=0, const int denom=1) {
 		const int f = gcd(num, denom);
-		numerator = num / f;
-		denominator = denom / f;
+		this->num = num / f;
+		this->denom = denom / f;
 
-		if (denominator == 0) {
+		if (denom == 0) {
 			throw std::invalid_argument("Denominator cannot be 0!");
-		} else if (denominator < 0) {
-			// Sign information is carried by the numerator,
-			// so switch the signs of both `numerator` and `denominator`.
-			numerator *= -1;
-			denominator *= -1;
+		} else if (denom < 0) {
+			// Sign information is carried by the num,
+			// so switch the signs of both `num` and `denom`.
+			this->num *= -1;
+			this->denom *= -1;
 		}
 	}
 
@@ -38,40 +53,72 @@ public:
 	Rational(const Rational&) = default;
 	Rational& operator =(const Rational&) = default;
 	~Rational() = default;
-
-	Rational add(const Rational& r) const {
-		return Rational{
-			numerator * r.denominator + r.numerator * denominator,
-			denominator * r.denominator
-		};
-	}
-
-	Rational sub(const Rational& r) const {
-		return Rational{
-			numerator * r.denominator - r.numerator * denominator,
-			denominator * r.denominator
-		};
-	}
-
-	Rational mutiply(const Rational& r) const {
-		return Rational{
-			numerator * r.numerator,
-			denominator * r.denominator
-		};
-	}
-
-	Rational divide(const Rational& r) const {
-		return Rational{
-			numerator * r.denominator,
-			r.numerator * denominator
-		};
-	}
 };
+
+
+// Arithmetic opetations
+Rational operator +(const Rational& left, const Rational& right) {
+	return Rational{
+		left.num * right.denom + right.num * left.denom,
+		left.denom * right.denom
+	};
+}
+
+Rational operator -(const Rational& left, const Rational& right) {
+	return Rational{
+		left.num * right.denom - right.num * left.denom,
+		left.denom * right.denom
+	};
+}
+
+Rational operator *(const Rational& left, const Rational& right) {
+	return Rational{
+		left.num * right.num,
+		left.denom * right.denom
+	};
+}
+
+Rational operator /(const Rational& left, const Rational& right) {
+	return Rational{
+		left.num * right.denom,
+		right.num * left.denom
+	};
+}
+
+
+// Comparison operators
+bool operator ==(const Rational& left, const Rational& right) {
+	return left.num == right.num && left.denom == right.denom;
+}
+
+bool operator !=(const Rational& left, const Rational& right) {
+	return !(left == right);
+}
+
+bool operator <(const Rational& left, const Rational& right) {
+	const int l = left.num * right.denom;
+	const int r = right.num * left.denom;
+	return l < r;
+}
+
+bool operator <=(const Rational& left, const Rational& right) {
+	const int l = left.num * right.denom;
+	const int r = right.num * left.denom;
+	return l <= r;
+}
+
+bool operator >=(const Rational& left, const Rational& right) {
+	return !(left < right);
+}
+
+bool operator >(const Rational& left, const Rational& right) {
+	return !(left <= right);
+}
 
 
 // Write a number to an `ostream` (for ex. `std::cout`)
 std::ostream& operator <<(std::ostream& os, const Rational& r) {
-	os << r.numerator << '/' << r.denominator;
+	os << r.num << '/' << r.denom;
 	return os;
 }
 
@@ -81,15 +128,30 @@ int main() {
 
 	std::cout << "r1 = " << r1 << ", r2 = " << r2 << std::endl;
 	std::cout << std::endl;
-	std::cout << "r1 + r2 = " << r1.add(r2) << std::endl;
-	std::cout << "r1 - r2 = " << r1.sub(r2) << std::endl;
-	std::cout << "r1 * r2 = " << r1.mutiply(r2) << std::endl;
-	std::cout << "r1 / r2 = " << r1.divide(r2) << std::endl;
+	std::cout << "r1 + r2 = " << r1 + r2 << std::endl;
+	std::cout << "r1 - r2 = " << r1 - r2 << std::endl;
+	std::cout << "r1 * r2 = " << r1 * r2 << std::endl;
+	std::cout << "r1 / r2 = " << r1 / r2 << std::endl;
 	std::cout << std::endl;
-	std::cout << "r1 + 1 = " << r1.add(1) << std::endl;
-	std::cout << "r1 - 2 = " << r1.sub(2) << std::endl;
-	std::cout << "r1 * 3 = " << r1.mutiply(3) << std::endl;
-	std::cout << "r1 / 4 = " << r1.divide(4) << std::endl;
+	std::cout << "r1 + 1 = " << r1 + 1 << std::endl;
+	std::cout << "r1 - 2 = " << r1 - 2 << std::endl;
+	std::cout << "r1 * 3 = " << r1 * 3 << std::endl;
+	std::cout << "r1 / 4 = " << r1 / 4 << std::endl;
+	std::cout << std::endl;
+	std::cout << "1 + r1 = " << 1 + r1 << std::endl;
+	std::cout << "2 - r1 = " << 2 - r1 << std::endl;
+	std::cout << "3 * r1 = " << 3 * r1 << std::endl;
+	std::cout << "4 / r1 = " << 4 / r1 << std::endl;
+	std::cout << std::endl;
+	std::cout << "r1 == r1 = " << (r1 == r1) << std::endl;
+	std::cout << "r2 == r2 = " << (r2 == r2) << std::endl;
+	std::cout << "r1 == r2 = " << (r1 == r2) << std::endl;
+	std::cout << "r1 != r2 = " << (r1 != r2) << std::endl;
+	std::cout << std::endl;
+	std::cout << "r1 < r2 = " << (r1 < r2) << std::endl;
+	std::cout << "r1 <= r2 = " << (r1 <= r2) << std::endl;
+	std::cout << "r1 >= r2 = " << (r1 >= r2) << std::endl;
+	std::cout << "r1 > r2 = " << (r1 > r2) << std::endl;
 
 	return 0;
 }
--- 2023-03-14/vector.cpp
+++ 2023-03-14/vector.cpp
@@ -2,7 +2,10 @@
 #include <stdexcept>
 #include <cassert>
 
+
 const size_t INITIAL_CAPACITY = 10;
+const size_t VECTOR_VALUE_NOT_FOUND = -1ull;
+
 
 class IntVector {
 private:
@@ -10,32 +13,66 @@ private:
 	size_t capacity;
 	int* array;
 
+	void allocate(const size_t capacity) {
+		// N.B! `array` is not freed by this method!
+		// Callers should ensure no mem. leaks occur!
+		this->capacity = capacity;
+		this->array = new int[capacity];
+	}
+
 	void cleanup() {
 		delete[] array;
 	}
 
-	void copy(const IntVector& that) {
-		count = that.count;
-		capacity = that.capacity;
-		array = new int[capacity];
-		for(size_t i = 0; i < count; i++) {
-			array[i] = that.array[i];
+	void copy(const int *arr, const size_t n) {
+		assert(n <= capacity);
+		for(size_t i = 0; i < n; i++) {
+			array[i] = arr[i];
+		}
+		count = n;
+	}
+
+	void checkAndIncreaseCapacity() {
+		assert (count <= capacity);
+		if (count == capacity) {
+			int *oldArray = array;
+			allocate(2 * capacity);
+			copy(oldArray, count);
+			delete[] oldArray;
 		}
 	}
 
 public:
-	IntVector(): count{0}, capacity{INITIAL_CAPACITY} {
-		array = new int[capacity];
+	IntVector(): count{0} {
+		allocate(INITIAL_CAPACITY);
 	}
 
 	IntVector(const IntVector& that) {
-		copy(that);
+		allocate(that.capacity);
+		copy(that.array, that.count);
+	}
+
+	IntVector(IntVector&& that):
+		count{that.count}, capacity{that.capacity}, array{that.array} {
+		// Set to `nullptr` otherwise it will be freed by `that`'s dtor!
+		that.array = nullptr;
 	}
 
 	IntVector& operator =(const IntVector& that) {
 		if (this != &that) {
 			cleanup();
-			copy(that);
+			allocate(that.capacity);
+			copy(that.array, that.count);
+		}
+		return *this;
+	}
+
+	IntVector& operator =(IntVector&& that) {
+		if (this != &that) {
+			count = that.count;
+			capacity = that.capacity;
+			array = that.array;
+			that.array = nullptr;
 		}
 		return *this;
 	}
@@ -52,6 +89,50 @@ public:
 		return size() == 0;
 	}
 
+	// Add elements
+	void append(const int value) {
+		checkAndIncreaseCapacity();
+		array[count] = value;
+		count++;
+	}
+
+	void insert(const size_t pos, const int value) {
+		if (pos >= count) {
+			throw std::out_of_range("Trying to insert element after the end!");
+		}
+		checkAndIncreaseCapacity();
+
+		for(size_t i = count; i > pos; i--) {
+			array[i] = array[i - 1];
+		}
+		array[pos] = value;
+		count++;
+	}
+
+	// Remove elements
+	void pop() {
+		if (isEmpty()) {
+			throw std::out_of_range("Trying to remove element from empty vector!");
+		}
+		count--;
+	}
+
+	void remove(const size_t pos) {
+		if (pos >= count) {
+			throw std::out_of_range("Trying to remove element after the end!");
+		}
+
+		for (size_t i = pos + 1; i < count; i++) {
+			array[i - 1] = array[i];
+		}
+		count--;
+	}
+
+	void clear() {
+		count = 0;
+	}
+
+	// Element getter and setter
 	int get(const size_t pos) const {
 		if (pos >= count) {
 			throw std::out_of_range("Trying to access element after the end!");
@@ -59,56 +140,76 @@ public:
 		return array[pos];
 	}
 
-	void append(const int value) {
-		assert (count <= capacity);
-		if (count == capacity) {
-			int *newArray = new int[2 * capacity];
-			capacity *= 2;
-			for (size_t i = 0; i < count; i++) {
-				newArray[i] = array[i];
+	void set(const size_t pos, const int value) {
+		if (pos >= count) {
+			throw std::out_of_range("Trying to access element after the end!");
+		}
+		array[pos] = value;
+	}
+
+	size_t find(const int value) const {
+		for (size_t i = 0; i < count; i++) {
+			if (array[i] == value) {
+				return i;
 			}
-			delete[] array;
-			array = newArray;
 		}
-
-		array[count] = value;
-		count++;
+		return VECTOR_VALUE_NOT_FOUND;
 	}
 
-	void pop() {
-		if (isEmpty()) {
-			throw std::out_of_range("Trying to remove element from empty vector!");
-		}
-		count--;
-	}
 };
 
 
-void print(const IntVector& v) {
-	for(size_t i = 0; i < v.size(); i ++) {
+void print(const char *name, const IntVector& v) {
+	std::cout << name << ": ";
+	for (size_t i = 0; i < v.size(); i++) {
 		std::cout << v.get(i) << ' ';
 	}
 	std::cout << std::endl;
 }
 
 int main() {
-	IntVector v{};
-	print(v);
+	// Create empty vector
+	IntVector v1{};
+	print("v1", v1);
 
+	// Check that trying get an element from an empty vector raises an exception
 	try {
-		v.get(1);
+		v1.get(0);
 		assert (false);
 	} catch (const std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
 
-	for(int i = 1; i < 15; i ++) {
-		v.append(i);
-		print(v);
+	// Fill it up with values.
+	for(int i = 0; i < 15; i ++) {
+		v1.append(i);
+		print("v1", v1);
 	}
 
-	IntVector v2{};
-	v = v2;
+	// Call copy constructor
+	IntVector v2{v1};
+	print("v2", v2);
+
+	// Remove element from
+	v2.remove(10);
+	print("v2", v2);
+	v2.insert(10, -1);
+	print("v2", v2);
+
+	// Check that v1 has **not** been changed!
+	print("v1", v1);
+
+	// Call assignment operator
+	IntVector v3{};
+	v3 = v1;
+	print("v3", v3);
+
+	v3.pop();
+	print("v3", v3);
+
+	for (const int i: {1, 3, 5, 7, 17}) {
+		std::cout << "v3.find(" << i << ") = " << v3.find(i) << std::endl;
+	}
 
 	return 0;
 }
